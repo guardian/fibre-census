317593e2d048dbe739110225afabe2a2c8dbaab8
Andy Gallagher <andy.gallagher@theguardian.com>
2018-11-26 14:21:12 +0000

    grrr

--- frontend/app/FrontPage.jsx
+++ frontend/app/FrontPage.jsx
@@ -180,7 +180,7 @@ class FrontPage extends React.Component {
                     getTrProps={this.rowValidationProps}
                     minRows={this.state.data.length}
                     defaultPageSize={this.state.data.length}
-                    pageSizeOptions={[100]}
+                    // pageSizeOptions={[100]}
                 />
             }
             </div>
