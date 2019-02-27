b7427aa5c210532a7ce715ca3bc1caf67d966ac3
Andy Gallagher <andy.gallagher@theguardian.com>
2019-02-27 10:22:58 +0000

    WIP

--- app/services/MailerActor.scala
+++ app/services/MailerActor.scala
@@ -5,7 +5,7 @@ import java.util.{Date, Properties}
 
 import akka.actor.Actor
 import javax.inject.Inject
-import javax.mail.{Address, Message, Session, Transport}
+import javax.mail._
 import models.Email
 import javax.mail.internet._
 import play.api.{Configuration, Logger}
@@ -14,22 +14,54 @@ import scala.collection.JavaConverters._
 import scala.util.{Failure, Success, Try}
 
 object MailerActor {
+
+  /*public input messages*/
   case class SendEmail(toSend:Seq[Email])
+
+  /*internal input messages*/
+  protected case class InternalSend(msg:MimeMessage)
 }
 
 class MailerActor @Inject() (config:Configuration) extends Actor {
   import MailerActor._
   private val logger = Logger(getClass)
 
-  override def receive: Receive = {
-    case SendEmail(toSend)=>
-      //set up an email session
-      val properties = new Properties()
-      properties.put("mail.smtp.host", config.get("email.smtpHost"))
-      val session = Session.getDefaultInstance(properties, null)
+  protected val myRef = self
 
+  def getMailerSession = {
+    val properties = new Properties()
+    properties.put("mail.smtp.host", config.get("email.smtpHost"))
+    Session.getDefaultInstance(properties, null)
+  }
+
+  private val session = getMailerSession
+
+  override def receive: Receive = {
+    case InternalSend(msg)=>
       val maybeSmtpUser = config.getOptional[String]("email.smtpUser")
       val maybeSmtpPasswd = config.getOptional[String]("email.smtpPassword")
+
+      val sendingResult = if(maybeSmtpPasswd.isDefined && maybeSmtpUser.isDefined) {
+        Try {Transport.send(msg, maybeSmtpUser.get, maybeSmtpPasswd.get)}
+      } else {
+        Try {Transport.send(msg)}
+      }
+
+      sendingResult match {
+        case Success(_)=>
+          logger.info(s"Sent message ${msg.getSubject} successfully")
+          sender() ! akka.actor.Status.Success
+        case Failure(err)=>
+          logger.error("Could not send message: ", err)
+          sender() ! akka.actor.Status.Failure(err)
+      }
+
+    /**
+      * dispatched by a caller to send a bunch of emails. We use a more scala-ish format to input,
+      * so this converts that into the Java-ish format that javax expects and then dispatches InternalSend to
+      * actually do the deed
+      */
+    case SendEmail(toSend)=>
       val javaMsgList = toSend.map(myMsg=> Try {
         val msg = new MimeMessage(session)
         msg.setRecipients(Message.RecipientType.TO,myMsg.to.toArray.map(_.asInstanceOf[Address]))
@@ -46,13 +78,10 @@ class MailerActor @Inject() (config:Configuration) extends Actor {
         logger.error("The following messages refused to build:")
         errors.foreach(err=>logger.error("Message: ", err))
       }
+
       val msgsToSend = javaMsgList.collect({case Success(msg)=>msg})
       if(msgsToSend.nonEmpty){
-        val sendResults = if(maybeSmtpPasswd.isDefined && maybeSmtpUser.isDefined) {
-          msgsToSend.map(msg=>Try {Transport.send(msg, maybeSmtpUser.get, maybeSmtpPasswd.get)})
-        } else {
-          msgsToSend.map(msg=>Try {Transport.send(msg)})
-        }
+        msgsToSend.foreach(msg=>myRef.tell(InternalSend(msg), sender()))
       } else {
         logger.error("No messages to send")
       }
