462a7cde797d1b4ed6e1a3e9d304fdd8a5609aaa
Andy Gallagher <andy.gallagher@theguardian.com>
2019-05-29 14:57:23 +0100

    adding log rotation config for clients as per https://serverfault.com/questions/352942/equivalent-of-logrotate-on-osx

--- client_script/install.sh
+++ client_script/install.sh
@@ -3,6 +3,8 @@
 cp profile_reader.pl /usr/local/bin
 cp fibrecensus-launcher.plist /Library/LaunchDaemons/com.gu.fibrecensus-launcher.plist
 
+cp newsyslog.conf /etc/newsyslog.d/fibrecensus.conf
+
 cd /Library/LaunchDaemons
 launchctl load com.gu.fibrecensus-launcher.plist
 launchctl start com.gu.fibrecensus-launcher.plist
