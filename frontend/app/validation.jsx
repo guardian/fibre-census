68ad9ad13f5d5a3fcca77637fe577ebe2e6244b6
Andy Gallagher <andy.gallagher@theguardian.com>
2019-05-30 15:50:10 +0100

    trip global alerts for san mounts and MDC ping

--- frontend/app/validation.jsx
+++ frontend/app/validation.jsx
@@ -4,6 +4,26 @@ const mustHaveVolumes = [
     "StudioPipe2"
 ];
 
+function validateMdcPing(mdcPing){
+    const visibleMdcList = mdcPing.filter(entry=>entry.visible);
+    if(visibleMdcList.length===0){
+        this.setState({tooltip: "No metadata controllers visible"});
+        return "warning";
+    }
+
+    if(visibleMdcList.length!==mdcPing.length){
+        this.setState({tooltip: "Not all metadata controllers visible"});
+        return "info";
+    }
+
+    const highPacketCounts = mdcPing.filter(entry=>entry.packetloss>0);
+    if(highPacketCounts.length>0){
+        this.setState({tooltip: "Packet loss seen, check network connections"});
+        return "info";
+    }
+    return "normal";
+}
+
 function validateRecord(record){
     console.debug("validateRecord", record);
     if(record.model!=="Mac Pro"){
@@ -20,10 +40,12 @@ function validateRecord(record){
         return "warning";
     }
 
+    if(!record.mdcPing) return "info";
+    const mdcStatus = validateMdcPing(record.mdcPing);
+    if(mdcStatus!=="normal") return "normal";
+
+
     const actualLunCount = record.fcLunCount.filter(entry=>entry>0);
-    if(actualLunCount[0]===120){
-        return "warning";
-    }
 
     if(actualLunCount[0]!==20){
         console.log(record.hostName + " only has " + actualLunCount + " LUNs visible (expected 20)");
@@ -32,6 +54,12 @@ function validateRecord(record){
 
     if(!record.denyDlcVolumes) return "info";
     if(record.denyDlcVolumes.length<mustHaveVolumes.length) return "info";
+
+    const sanMountsNames = record.sanMounts.map(entry=>entry.name);
+    if(sanMountsNames.length<mustHaveVolumes.length) return "info";
+
+    if(!record.sanMounts) return "info";
+
 
     if(mustHaveVolumes.filter(entry=>!record.denyDlcVolumes.includes(entry)).length>0) return "info";
     return "normal";
