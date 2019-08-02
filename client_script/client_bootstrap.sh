d717be1ca42cad1b0fcb55553823e01d8ef2945f
Andy Gallagher <andy.gallagher@theguardian.com>
2019-08-02 10:19:14 +0100

    adding a "bootstrap" script that downloads and installs the zip

--- client_script/client_bootstrap.sh
+++ client_script/client_bootstrap.sh
@@ -1,5 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
 
 #Downloads and installs the client-side agent. This should be run as root, and assumes that curl is installed.
 
-curl https://multimedia-public-downloadables.s3-eu-west-1.amazonaws.com/fibrecensus/master/fibrecensus-client.zip
+mkdir -p /tmp/fibrecensus-client
+cd /tmp/fibrecensus-client
+curl https://multimedia-public-downloadables.s3-eu-west-1.amazonaws.com/fibrecensus/master/fibrecensus-client.zip > fibrecensus-client.zip
+unzip fibrecensus-client.zip
+
+bash ./install.sh
+
+cd
+rm -rf /tmp/fibrecensus-client
