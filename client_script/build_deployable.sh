52f281efae68e566d90bfe36fdd7d7a31ae7e61b
Andy Gallagher <andy.gallagher@theguardian.com>
2019-08-02 09:56:12 +0100

    doh

--- client_script/build_deployable.sh
+++ client_script/build_deployable.sh
@@ -20,7 +20,7 @@ $SHACMD fibrecensus-client-${BUILD_NUM}.zip > fibrecensus-client-${BUILD_NUM}.sh
 aws s3 cp fibrecensus-client-${BUILD_NUM}.zip s3://multimedia-public-downloadables/fibrecensus/${BUILD_NUM}/fibrecensus-client-${BUILD_NUM}.zip
 aws s3 cp fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadables/fibrecensus/${BUILD_NUM}/fibrecensus-client-${BUILD_NUM}.sha
 
-if [ "${BUILD_BRANCH}" == "master" ]; then
+if [ "${BUILD_BRANCH}" == "refs/heads/master" ]; then
     aws s3 cp fibrecensus-client-${BUILD_NUM}.zip s3://multimedia-public-downloadables/fibrecensus/master/fibrecensus-client.zip
     aws s3 cp fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadables/fibrecensus/master/fibrecensus-client.sha
 fi
