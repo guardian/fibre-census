1f45de0de6aa3b258162673d89fa949025240692
Andy Gallagher <andy.gallagher@theguardian.com>
2019-08-02 10:04:08 +0100

    fix upload ACL

--- client_script/build_deployable.sh
+++ client_script/build_deployable.sh
@@ -17,10 +17,12 @@ echo building number ${BUILD_NUM} on ${BUILD_BRANCH}
 zip fibrecensus-client-${BUILD_NUM}.zip profile_reader.pl fibrecensus-launcher.plist install.sh
 $SHACMD fibrecensus-client-${BUILD_NUM}.zip > fibrecensus-client-${BUILD_NUM}.sha
 
-aws s3 cp fibrecensus-client-${BUILD_NUM}.zip s3://multimedia-public-downloadables/fibrecensus/${BUILD_NUM}/fibrecensus-client-${BUILD_NUM}.zip
-aws s3 cp fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadables/fibrecensus/${BUILD_NUM}/fibrecensus-client-${BUILD_NUM}.sha
+echo Starting upload, AWS profile is ${AWS_PROFILE}
+
+aws s3 cp --acl public-read fibrecensus-client-${BUILD_NUM}.zip s3://multimedia-public-downloadables/fibrecensus/${BUILD_NUM}/fibrecensus-client-${BUILD_NUM}.zip
+aws s3 cp --acl public-read fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadables/fibrecensus/${BUILD_NUM}/fibrecensus-client-${BUILD_NUM}.sha
 
 if [ "${BUILD_BRANCH}" == "refs/heads/master" ]; then
-    aws s3 cp fibrecensus-client-${BUILD_NUM}.zip s3://multimedia-public-downloadables/fibrecensus/master/fibrecensus-client.zip
-    aws s3 cp fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadables/fibrecensus/master/fibrecensus-client.sha
+    aws s3 cp --acl public-read fibrecensus-client-${BUILD_NUM}.zip s3://multimedia-public-downloadables/fibrecensus/master/fibrecensus-client.zip
+    aws s3 cp --acl public-read fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadables/fibrecensus/master/fibrecensus-client.sha
 fi
