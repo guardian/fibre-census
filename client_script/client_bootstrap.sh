60da869d8003ba81d74e6c399604d1e42fe70a1a
Andy Gallagher <andy.gallagher@theguardian.com>
2019-08-02 10:42:21 +0100

    set correct server url when using bootstrap, and add precautionary unload to installer

--- client_script/client_bootstrap.sh
+++ client_script/client_bootstrap.sh
@@ -2,10 +2,18 @@
 
 #Downloads and installs the client-side agent. This should be run as root, and assumes that curl is installed.
 
+if [ "$1" == "" ]; then
+    echo You should specify the server to target as the first argument, i.e. ./client_bootstrap.sh https://my-server.company.co.uk
+    exit 1
+fi
+
 mkdir -p /tmp/fibrecensus-client
 cd /tmp/fibrecensus-client
 curl https://multimedia-public-downloadables.s3-eu-west-1.amazonaws.com/fibrecensus/master/fibrecensus-client.zip > fibrecensus-client.zip
 unzip fibrecensus-client.zip
+
+mv fibrecensus-launcher.plist fibrecensus-launcher.plist.old
+cat fibrecensus-launcher.plist.old | sed s?https://server-uri-here?1? > fibrecensus-launcher.plist
 
 bash ./install.sh
 
--- client_script/install.sh
+++ client_script/install.sh
@@ -1,8 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash
 
 cp profile_reader.pl /usr/local/bin
 cp fibrecensus-launcher.plist /Library/LaunchDaemons/com.gu.fibrecensus-launcher.plist
 
 cd /Library/LaunchDaemons
+launchctl unload com.gu.fibrecensus-launcher.plist  2>/dev/null #in case it hass already been started. Silence error to avoid confusion..
 launchctl load com.gu.fibrecensus-launcher.plist
 launchctl start com.gu.fibrecensus-launcher.plist
