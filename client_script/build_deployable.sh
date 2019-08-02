c91a5b96d9e7da3955d93a047db625e30bc8e13a
Andy Gallagher <andy.gallagher@theguardian.com>
2019-08-02 09:55:49 +0100

    bugfixes

--- client_script/build_deployable.sh
+++ client_script/build_deployable.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
 
+cd "$( dirname "${BASH_SOURCE[0]}" )"   #make sure we are in the client_script dir
+
+if [ -x $(which shasum) ]; then
+    SHACMD="shasum -a 256"
+else
+    SHACMD="sha256sum"
+fi
+
 if [ "${BUILD_NUM}" == "" ]; then
     BUILD_NUM="DEV"
 fi
@@ -7,7 +15,7 @@ fi
 echo building number ${BUILD_NUM} on ${BUILD_BRANCH}
 
 zip fibrecensus-client-${BUILD_NUM}.zip profile_reader.pl fibrecensus-launcher.plist install.sh
-shasum -a 256 fibrecensus-client-${BUILD_NUM}.zip > fibrecensus-client-${BUILD_NUM}.sha
+$SHACMD fibrecensus-client-${BUILD_NUM}.zip > fibrecensus-client-${BUILD_NUM}.sha
 
 aws s3 cp fibrecensus-client-${BUILD_NUM}.zip s3://multimedia-public-downloadables/fibrecensus/${BUILD_NUM}/fibrecensus-client-${BUILD_NUM}.zip
 aws s3 cp fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadables/fibrecensus/${BUILD_NUM}/fibrecensus-client-${BUILD_NUM}.sha
@@ -15,5 +23,4 @@ aws s3 cp fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadabl
 if [ "${BUILD_BRANCH}" == "master" ]; then
     aws s3 cp fibrecensus-client-${BUILD_NUM}.zip s3://multimedia-public-downloadables/fibrecensus/master/fibrecensus-client.zip
     aws s3 cp fibrecensus-client-${BUILD_NUM}.sha s3://multimedia-public-downloadables/fibrecensus/master/fibrecensus-client.sha
-
 fi
---
+++ client_script/client_bootstrap.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+#Downloads and installs the client-side agent. This should be run as root, and assumes that curl is installed.
+
+curl https://multimedia-public-downloadables.s3-eu-west-1.amazonaws.com/fibrecensus/master/fibrecensus-client.zip
