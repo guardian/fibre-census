b08829751e8968de502853c6a2ede0f59e598d46
andy_gallagher <andy.gallagher@theguardian.com>
2021-06-21 11:15:21 +0100

    fix incorrect global validation for MDC data and LUN count

--- frontend/app/validation/ValidateLunCount.jsx
+++ frontend/app/validation/ValidateLunCount.jsx
@@ -3,7 +3,7 @@ import ValidationComponent from "./ValidationComponent.jsx";
 class ValidateLunCount extends ValidationComponent {
     performValidation() {
         const matchingCounts = this.props.listData.filter(entry=>entry===40);
-        if(matchingCounts.length===40){
+        if(matchingCounts.length===0){
             this.setState({tooltip: "Expecting 40 LUNs visible on at least one interface"});
             return "warning";
         }
