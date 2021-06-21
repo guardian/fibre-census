387739960000e764a1e0ce043232741550b277ff
andy_gallagher <andy.gallagher@theguardian.com>
2021-06-21 11:04:32 +0100

    fix for current valid lun count

--- frontend/app/validation/ValidateLunCount.jsx
+++ frontend/app/validation/ValidateLunCount.jsx
@@ -2,7 +2,7 @@ import ValidationComponent from "./ValidationComponent.jsx";
 
 class ValidateLunCount extends ValidationComponent {
     performValidation() {
-        const matchingCounts = this.props.listData.filter(entry=>entry===20);
+        const matchingCounts = this.props.listData.filter(entry=>entry===40);
         if(matchingCounts.length===0){
             this.setState({tooltip: "Expecting 20 LUNs visible on at least one interface"});
             return "warning";
