7dda131b9fce7343197ba4a9474857773012aaaa
Dave Allison <david.allison@theguardian.com>
2023-11-24 15:44:27 +0000

    First attempt at adding Azure

---
+++ app/controllers/Login.scala
@@ -0,0 +1,44 @@
+package controllers
+
+import auth.{BearerTokenAuth, Security}
+import play.api.Configuration
+import play.api.cache.SyncCacheApi
+import play.api.libs.circe.Circe
+import play.api.libs.json.Json
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class Login @Inject()(override implicit val config:Configuration,
+                      override val bearerTokenAuth:BearerTokenAuth,
+                      cc:ControllerComponents
+                     )(override implicit val cache:SyncCacheApi)
+  extends AbstractController(cc) with Security with Circe {
+
+  /**
+    * Action that allows the frontend to test if the current session is valid
+    * @return If the session is not valid, a 403 response
+    *         If the session is valid, a 200 response with the currently logged in userid in a json object
+    */
+  def isLoggedIn = IsAuthenticated { uid=> { request=>
+    Ok(Json.obj("status"->"ok","uid"->uid))
+  }}
+
+  /**
+    * Action that allows the frontend to test if the user is an admin
+    * @return If the user is not an admin, a 403 response. If the user is an admin, a 200 response
+    */
+  def checkIsAdmin = IsAdmin {uid=> {request=>
+    Ok(Json.obj("status"->"ok"))
+  }}
+
+  /**
+    * Action to log out, by clearing the client's session cookie.
+    * @return
+    */
+  def logout = Action { request=>
+    Ok(Json.obj("status"->"ok","detail"->"Logged out")).withNewSession
+  }
+
+}
---
+++ app/controllers/OAuthMetaController.scala
@@ -0,0 +1,66 @@
+package controllers
+
+import io.circe.generic.auto._
+import io.circe.syntax._
+import org.slf4j.LoggerFactory
+import play.api.Configuration
+import play.api.libs.circe.Circe
+import play.api.mvc.{AbstractController, ControllerComponents}
+import responses.{GenericErrorResponse, OAuthConfigResponse}
+
+import javax.inject.{Inject, Singleton}
+import scala.io.Source
+
+@Singleton
+class OAuthMetaController @Inject() (config:Configuration, cc:ControllerComponents) extends AbstractController(cc) with Circe {
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  /**
+   * present the JWT signing key to the frontend so it can validate JWTs
+   * @return
+   */
+  def signingKey = Action {
+    config.getOptional[String]("auth.tokenSigningCertPath") match {
+      case None=>
+        InternalServerError(GenericErrorResponse("config_error","No signing cert configured on server").asJson)
+      case Some(certPath)=>
+        try {
+          var content = ""
+          if (certPath.startsWith("http")) {
+            content = "Not set up to read a local file."
+          } else {
+            val src = Source.fromFile(certPath, "UTF-8")
+            content = src.mkString
+            src.close()
+          }
+
+          Ok(content).as("application/x-x509-ca-cert")
+        } catch {
+          case err:Throwable=>
+            logger.error(s"Could not read signing cert at $certPath: ", err)
+            InternalServerError(GenericErrorResponse("error","Could not read signing cert, see server logs").asJson)
+        }
+    }
+  }
+
+  def oauthConfig = Action {
+    try {
+      val response = OAuthConfigResponse(
+        config.get[String]("auth.clientId"),
+        config.get[String]("auth.resource"),
+        config.get[String]("auth.oAuthUri"),
+        config.get[String]("auth.tokenUri"),
+        config.get[Seq[String]]("auth.validAudiences"),
+        config.get[String]("auth.adminClaimName"),
+        config.get[String]("auth.scope"),
+        config.get[String]("auth.tokenSigningCertPath"),
+      )
+
+      Ok(response.asJson)
+    } catch {
+      case err:Throwable=>
+        logger.error("Could not get oauth2 configuration: ", err)
+        InternalServerError(GenericErrorResponse("config_error","Oauth2 is not configurated correctly, consult the server logs").asJson)
+    }
+  }
+}
